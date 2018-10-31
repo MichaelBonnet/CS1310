2090f9220400b83ba608638e07d9d2c77259a1d7
Michael Bonnet <36342359+MichaelBonnet@users.noreply.github.com>
2018-10-31 13:09:57 -0500

    Added comments

--- mbonnet-assignment1.c
+++ mbonnet-assignment1.c
@@ -6,16 +6,16 @@
 
 #include <stdio.h>
 
-int main (void) {
-	float tempCelsius;
-	float tempFahrenheit;
+int main (void) { // Beginning of main
+	float tempCelsius; // Creates variable named tempCelsius; type float in order to create more exact calculations
+	float tempFahrenheit; // Creates variable named tempFahrenheit; type float in order to create more exact calculations
 
-	printf("Enter the temperature in degrees Celsius:____.");
-	scanf("%f", &tempCelsius);
+	printf("Enter the temperature in degrees Celsius:____."); // Prompts user for input
+	scanf("%f", &tempCelsius); // Gets user input and stores it as type float in variable tempCelsius
 	
-	tempFahrenheit = tempCelsius * (9.0 / 5.0) + 32.0;
+	tempFahrenheit = tempCelsius * (9.0 / 5.0) + 32.0; // Converts tempCelsius to Fahrenheit and stores it in variable tempFahrenheit
 
-	printf("The temperature in degrees Fahrenheit is %f.", tempFahrenheit);
+	printf("The temperature in degrees Fahrenheit is %f.", tempFahrenheit); // Displays the result of the calculation
 
-	return(0);
-}
+	return(0); // return statement
+} // End of main
