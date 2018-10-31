52bc08a29b8c99e9c0b3037ca703084adf1fd450
Michael Bonnet <36342359+MichaelBonnet@users.noreply.github.com>
2018-10-31 13:39:55 -0500

    Update mbonnet-assignment3.c
    added comments

--- mbonnet-assignment3.c
+++ mbonnet-assignment3.c
@@ -6,91 +6,102 @@
 
 #include <stdio.h>
 
-void FahrenheitToCelsiusConversion (double conversionValue) { // Converts Fahrenheit to Celsius
+void FahrenheitToCelsiusConversion (double conversionValue) // Converts Fahrenheit to Celsius
+{ // Beginning of function FahrenheitToCelsiusConversion
 	
-	double output;
+	double output; // initializes variable output to store the result of the conversion
 	
-	output = (conversionValue - 32) / (9.0 / 5.0);
+	output = (conversionValue - 32) / (9.0 / 5.0); // converts conversionValue from Fahrenheit to Celsius, stores the result in output
 	
-	printf("%.2lf", output);
-}
+	printf("%.2lf", output); // prints the result of the conversion
+	
+} // End of function FahrenheitToCelsiusConversion
 
-void CelsiusToFahrenheitConversion (double conversionValue) { // Converts Celsius to Fahrenheit
+void CelsiusToFahrenheitConversion (double conversionValue) // Converts Celsius to Fahrenheit
+{ // Beginning of function CelsiusToFahrenheitConversion
 	
-	double output;
+	double output; // initializes variable output to store the result of the conversion
 	
-	output = (conversionValue) * (9.0 / 5.0) + 32;
+	output = (conversionValue) * (9.0 / 5.0) + 32; // converts conversionValue from Celsius to Fahrenheit, stores the result in output
 	
-	printf("%.2lf", output);
-}
+	printf("%.2lf", output); // prints the result of the conversion
+	
+} // End of function CelsiusToFahrenheitConversion
 
-void InchesToCentimetersConversion (double conversionValue) { // Converts Inches to Centimeters
+void InchesToCentimetersConversion (double conversionValue) // Converts Inches to Centimeters
+{ // Beginning of fuction InchesToCentimetersConversion
 	
-	double output;
+	double output; // initializes variable output to store the result of the conversion
 	
-	output = conversionValue * 2.54;
+	output = conversionValue * 2.54; // converts conversionValue from Inches to Centimeters, stores the result in output
 	
-	printf("%.2lf", output);
-}
+	printf("%.2lf", output); // prints the result of the conversion
+	
+} // End of function InchesToCentimetersConveriosn
 
-void CentimetersToInchesConversion (double conversionValue) { // Converts Centimeters to Inches
+void CentimetersToInchesConversion (double conversionValue) // Converts Centimeters to Inches
+{ // Beginning of function CentimetersToInchesConversion
 	
-	double output;
+	double output; // initializes variable output to store the result of the conversion
 	
-	output = conversionValue / 2.54;
+	output = conversionValue / 2.54; // converts conversionValue from Centimeters to Inches, stores the result in output
 
-	printf("%.2lf", output);
-}
-
-int main(void) {
+	printf("%.2lf", output); // prints the result of the conversion
 	
-int conversionChoice;
-double conversionValue;
+} // End of function CentimetersToInchesConversion
 
-printf("Select from the menu below to convert temperature or linear\n");
-printf("menus:\n");
-printf("\n");
-printf("1 Convert Fahrenheit to Celsius\n");
-printf("2 Convert Celsius to Fahrenheit\n");
-printf("3 Convert Inches to Centimeters\n");
-printf("4 Convert Centimeters to Inches\n");
-printf("5 Exit the Program\n");
+int main(void) // Main function
+{ // Beginning of function main
+	
+	int conversionChoice; // initializes variable conversionChoice which will store user input and help determine what conversion to run
+	double conversionValue; // initializes variable conversionValue of type double to store user input and use to make an exact conversion calculation
 
-scanf("%d", &conversionChoice); 
+	// The gollowing print statements show the user a menu of their options, prompting the user for a response
+	printf("Select from the menu below to convert temperature or linear\n");
+	printf("menus:\n");
+	printf("\n");
+	printf("1 Convert Fahrenheit to Celsius\n");
+	printf("2 Convert Celsius to Fahrenheit\n");
+	printf("3 Convert Inches to Centimeters\n");
+	printf("4 Convert Centimeters to Inches\n");
+	printf("5 Exit the Program\n");
 
-printf("Enter the value you wish to convert:\n");
-scanf("%lf", &conversionValue); // Gets single conversion value to plug into any of the functions called by the switch statements
+	scanf("%d", &conversionChoice); // Gets user response and stores it in variable conversionChoice, used later to determine what conversion to run in a switch statement
 
-switch (conversionChoice) { // Determines which conversion to run
+	printf("Enter the value you wish to convert:\n"); // Prompts user for a value to convert
+	scanf("%lf", &conversionValue); // Stores user value into conversionValue, will be passed into functions based on conversion choice
 	
-case 1:
-	FahrenheitToCelsiusConversion(conversionValue);
-	break;
+	switch (conversionChoice) // Determines which conversion to run
+	{ // Beginning of switch statement
+	
+	case 1: // If conversionChoice = 1, conversionValue will be converted from Fahrenheit to Celsius.
+		FahrenheitToCelsiusConversion(conversionValue); // Calls function FahrenheitToCelsiusConversion and passes conversionValue into it
+		break;
 
-case 2:
-	CelsiusToFahrenheitConversion(conversionValue);
-	break;
+	case 2: // If conversionChoice = 2, conversionValue will be converted from Celsius to Fahrenheit.
+		CelsiusToFahrenheitConversion(conversionValue); // Calls function CelsiusToFahrenheitConversion and passes conversionValue into it
+		break;
 	
-case 3:
-	InchesToCentimetersConversion(conversionValue);
-	break;
+	case 3: // If conversionChoice = 3, conversionValue will be converted from Inches to Centimeters.
+		InchesToCentimetersConversion(conversionValue); // Calls function InchesToCentimetersConversion and passes conversionValue into it
+		break;
 	
-case 4:
-	CentimetersToInchesConversion(conversionValue);
-	break;
+	case 4: // If conversionChoice = 4, conversionValue will be converted from Centimeters to Inches.
+		CentimetersToInchesConversion(conversionValue); // Calls function CentimetersToInchesConversion and passes conversionValue into it
+		break;
 	
-case 5:
-	printf("Program exiting.");
-	break;
+	case 5: // If conversionChoice = 5, the program will exit.
+		printf("Program exiting.");
+		break;
 	
 	
-default:
-	printf("Invalid choice. Program exiting."); // In case the user inputs an invalid number.
-	break;
+	default: // If the user chose an invalid integer in the menu prompt, the user will be informed that the choice was invalid and that the program is exiting.
+		printf("Invalid choice. Program exiting."); // Informs the user of their invalid choice and of program exiting.
+		break;
 
-}
+	} // End of switch statement
 
-return 0;	
+	return 0; // return statement	
 	
-}
+} // End of function main
 
