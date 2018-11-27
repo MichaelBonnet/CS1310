cb482661cc8357c38209ea0b58b032c150159687
Michael Bonnet <36342359+MichaelBonnet@users.noreply.github.com>
2018-11-27 01:34:43 -0600

    Update mbonnet-assignment6.c

--- mbonnet-assignment6.c
+++ mbonnet-assignment6.c
@@ -3,8 +3,8 @@
 #include <stdbool.h>
 
 struct course {
-   const char *courseName;
-   char registeredStudents[25][50];
+   const char *courseName[50];
+   char registeredStudents[1][25];
 
 } course;
 
@@ -22,10 +22,10 @@ int main(void)
 	struct course ObjectOrientedProgrammingI;
 	struct course ObjectOrientedProgrammingII;
 	
-	ProgrammingI.courseName = "Programming I";
-	ProgrammingII.courseName = "Programming II";
-	ObjectOrientedProgrammingI.courseName = "Object Oriented Programming I";
-	ObjectOrientedProgrammingII.courseName = "Object Oriented Programming II";
+	strncpy(*ProgrammingI.courseName, "Programming I", 50);
+    strncpy(*ProgrammingII.courseName, "Programming II", 50);
+    strncpy(*ObjectOrientedProgrammingI.courseName, "Object Oriented Programming I", 50);
+    strncpy(*ObjectOrientedProgrammingII.courseName, "Object Oriented Programming II", 50);
 	
 	for (i = 0; i < 25; i++)
 		{
@@ -57,14 +57,14 @@ int main(void)
 		printf("Type r to register for a course\n");
 		printf("Type s to list registered students in a course\n");
 		printf("Type q to quit the program\n");
-		scanf("%c", userChoice);
+		scanf("%c", &userChoice);
 	
 		if (userChoice == 'l')
 		{
-			printf("%c", ProgrammingI.courseName);
-			printf("%c", ProgrammingII.courseName);
-			printf("%c", ObjectOrientedProgrammingI.courseName);
-			printf("%c", ObjectOrientedProgrammingII.courseName);
+			printf("%s\n", ProgrammingI.courseName);
+			printf("%s\n", ProgrammingII.courseName);
+			printf("%s\n", ObjectOrientedProgrammingI.courseName);
+			printf("%s\n", ObjectOrientedProgrammingII.courseName);
 		}
 			else if (userChoice == 'r')
 			{
@@ -73,10 +73,10 @@ int main(void)
 				printf("Type 2 for Programming II.\n");
 				printf("Type 3 for Object Oriented Programming I.\n");
 				printf("Type 4 for Object Oriented Programming II.\n");
-				scanf("%c", userCourse);
+				scanf("%c", &userCourse);
 				
 				printf("Please type the name of the student being registered for the course.");
-				scanf("%c", studentName);
+				scanf("%c", &studentName);
 				
 				if (userCourse == 1)
 				{
@@ -130,7 +130,7 @@ int main(void)
 					printf("Type 2 for Programming II.\n");
 					printf("Type 3 for Object Oriented Programming I.\n");
 					printf("Type 4 for Object Oriented Programming II.\n");
-					scanf("%c", userCourse);
+					scanf("%c", &userCourse);
 					
 					if (userCourse == 1)
 					{
